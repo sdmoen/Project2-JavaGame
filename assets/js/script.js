b7e3bc0ba7c1de5f83febd35a70420fd84db754e
sdmoen <sdmoen@outlook.com>
2021-11-14 23:18:32 +0000

    add java script structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,38 @@ const rock_div = document.getElementById('rock');
 const paper_div = document.getElementById('paper');
 const scissors_div = document.getElementById('scissors');
 
+// function for random pick from computer
+function(){
+
+}
+
+// function for winner with on screen word description.
+function(){
+
+}
+
+//function for losing with on screen word description.
+function(){
+
+}
+
+
+// function for draw with on screen word description.
+function(){
+
+}
+
+// function for player vs computer outcomes using switch statements to 
+//  compare values of each case.
+function (){
+    const
+    switch
+    case
+    case
+    case
+
+    break;
+}
+
+//function for main game using eventlistener with event 'click'
+function 
