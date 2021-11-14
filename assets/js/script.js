0448c2ab93fc79ee3d5ee8441a180b4c7839159c
sdmoen <sdmoen@outlook.com>
2021-11-14 23:04:39 +0000

    add java link and tested, add initial java DOM variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log('hello');
+
+var userScore = 0;
+var computerScore = 0;
+var userScore_span = document.getElementById('user-score');
+var computerScore_span = document.getElementById('computer-score');
+const scoreBoard_div = document.querySelector('.score-board');
+const result_div = document.querySelector('.result >p');
+const rock_div = document.getElementById('rock');
+const paper_div = document.getElementById('paper');
+const scissors_div = document.getElementById('scissors');
+
