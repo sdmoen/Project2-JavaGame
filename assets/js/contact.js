832e4548722103414d9827d607448b4b5e8a3f69
sdmoen <sdmoen@outlook.com>
2021-11-17 20:21:27 +0000

    add user id to emailjs.init

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMcontentLoaded', function(){
     DocumentType.getElementbyId('form').addEventListener('submit',function(event){
         event.preventDefault();
-        emailjs.init("service_0gpeva2");
+        emailjs.init("user_855GYOKzNke21zoGuscrq");
         emailjs.sendForm('form', this)
         .then(function(){
             console.log('SUCCESS!');
