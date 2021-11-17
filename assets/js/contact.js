38ecb3e92a1db7f4cab25b0bf30aca9da38e8ce4
sdmoen <sdmoen@outlook.com>
2021-11-17 20:19:44 +0000

    add emailjs functions

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMcontentLoaded', function(){
-    DocumentType.getElementbyId
+    DocumentType.getElementbyId('form').addEventListener('submit',function(event){
+        event.preventDefault();
+        emailjs.init("service_0gpeva2");
+        emailjs.sendForm('form', this)
+        .then(function(){
+            console.log('SUCCESS!');
+        }, function(error) {
+            consolwe.log('FAILED...', error);
+        }
+        )
+    })
 })
