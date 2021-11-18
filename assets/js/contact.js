b608898b720a709070361456e30a98eefb3f6de4
sdmoen <sdmoen@outlook.com>
2021-11-18 10:47:33 +0000

    add contact.js script and tested success

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,13 +1,19 @@
-document.addEventListener('DOMcontentLoaded', function(){
-    document.getElementbyId('form').addEventListener('submit', function (event){
+(function() {
+    // https://dashboard.emailjs.com/admin/integration
+    emailjs.init('user_855GYOKzNke21zoGuscrq');
+})();
+
+window.onload = function() {
+    document.getElementById('form').addEventListener('submit', function(event) {
         event.preventDefault();
-        emailjs.init("user_855GYOKzNke21zoGuscrq");
-        emailjs.sendForm('service_0gpeva2','template_w8a42b5' ,this)
-        .then(function(){
-            console.log('SUCCESS!');
-        }, function(error) {
-            console.log('FAILED...', error);
-        }
-        )
-    })
-})
+        // generate a five digit number for the contact_number variable
+        // this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('service_0gpeva2','template_w8a42b5', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
